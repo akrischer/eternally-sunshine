07e41e0b11f8d40460a0eb25cb9f415a0627729c
akrischer <krischer.a@husky.neu.edu>
2017-09-20 13:26:32 -0400

    Add main_menu scene and accompanying scripts

---
+++ Assets/Scripts/MainMenu/ContinuousWalking.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
+
+[RequireComponent(typeof(ThirdPersonCharacter))]
+public class ContinuousWalking : MonoBehaviour {
+
+    private Vector3 startPosition;
+    ThirdPersonCharacter thirdPersonCharacter;
+
+	// Use this for initialization
+	void Start () {
+        startPosition = transform.position;
+        // get the third person character ( this should never be null due to require component )
+        thirdPersonCharacter = GetComponent<ThirdPersonCharacter>();
+    }
+	
+	void FixedUpdate()
+    {
+        Vector3 moveVector = Vector3.forward;
+        Vector3 reverseMoveVector = -moveVector;
+        thirdPersonCharacter.Move(Vector3.forward, false, false);
+        transform.position = startPosition;
+    }
+}
---
+++ Assets/Scripts/Util/Rotate.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotate : MonoBehaviour {
+
+    public List<Rotation> rotations;
+
+	void FixedUpdate()
+    {
+        foreach (Rotation rotation in rotations)
+        {
+            Vector3 rotationVector = Vector3.zero;
+            switch(rotation.rotationAxis)
+            {
+                case Rotation.RotationAxis.X:
+                    rotationVector = Vector3.right;
+                    break;
+                case Rotation.RotationAxis.Y:
+                    rotationVector = Vector3.up;
+                    break;
+                case Rotation.RotationAxis.Z:
+                    rotationVector = Vector3.forward;
+                    break;
+            }
+            transform.Rotate(rotationVector * rotation.degreesPerSecond * Time.fixedDeltaTime);
+        }
+    }
+
+    [System.Serializable]
+    public class Rotation
+    {
+        public enum RotationAxis
+        {
+            X, Y, Z
+        }
+
+        public RotationAxis rotationAxis;
+        public float degreesPerSecond;
+    }
+}
